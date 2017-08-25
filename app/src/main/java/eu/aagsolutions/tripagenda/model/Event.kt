db36d94aae073e92b488ca5e0deb43faeac99a38
avramesc <aurel.avramescu@tomtom.com>
2017-08-25 14:18:52 +0200

    Draft models

---
+++ app/src/main/java/eu/aagsolutions/tripagenda/model/Event.kt
@@ -0,0 +1,9 @@
+package eu.aagsolutions.tripagenda.model
+
+import java.util.Date
+
+
+/**
+ * Created by avramesc on 8/25/17.
+ */
+class Event(val location: GeoLocation, val startDateTime: Date, val duration: Int)
---
+++ app/src/main/java/eu/aagsolutions/tripagenda/model/GeoLocation.kt
@@ -0,0 +1,8 @@
+package eu.aagsolutions.tripagenda.model
+
+import java.math.BigDecimal
+
+/**
+ * Created by avramesc on 8/25/17.
+ */
+class GeoLocation(val address: String, val lon: BigDecimal, val lat: BigDecimal)
