01ba3cb55a61618cec6325a75f2a6efa890f31e2
avramesc <aurel.avramescu@tomtom.com>
2017-08-25 12:34:27 +0200

    Add activity for building the trip agenda

---
+++ app/src/main/java/eu/aagsolutions/tripagenda/AgendaActivity.kt
@@ -0,0 +1,12 @@
+package eu.aagsolutions.tripagenda
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AgendaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agenda)
+    }
+}
