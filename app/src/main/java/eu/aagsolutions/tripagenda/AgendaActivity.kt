61de1256f8e41ca02147e12e5308ace7c9c39443
atdi <aurel.avramescu@gmail.com>
2017-09-04 22:47:03 +0200

    Add dynamic input texts

--- app/src/main/java/eu/aagsolutions/tripagenda/AgendaActivity.kt
+++ app/src/main/java/eu/aagsolutions/tripagenda/AgendaActivity.kt
@@ -1,23 +1,18 @@
 package eu.aagsolutions.tripagenda
 
 import android.app.DatePickerDialog
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
+import android.support.v7.app.AppCompatActivity
 import android.widget.LinearLayout
-import kotlinx.android.synthetic.main.activity_agenda.*
-import java.util.*
+import kotlinx.android.synthetic.main.activity_agenda.addStop
+import kotlinx.android.synthetic.main.activity_agenda.btnDate
+import java.util.Calendar
 
 class AgendaActivity : AppCompatActivity() {
 
     private val startCalendar = Calendar.getInstance()
 
     private val startDateString = StringBuilder()
-
-    private val arrayList = ArrayList<LinearLayout>()
-
-    private var adapter: ArrayAdapter<LinearLayout>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +26,6 @@ class AgendaActivity : AppCompatActivity() {
         val year = c.get(Calendar.YEAR)
         startCalendar.set(year, month, day, hour, minute)
         startDateString.append(day).append("/").append(month).append("/").append(year)
-        adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, arrayList)
-        this.stopPoints.adapter = adapter
         this.btnDate.text = startDateString.toString()
 
         this.btnDate.setOnClickListener {
@@ -47,21 +40,27 @@ class AgendaActivity : AppCompatActivity() {
                                 .append(startMonth)
                                 .append("/")
                                 .append(startYear)
-                        btnDate.text = startDateString.toString()},
+                        btnDate.text = startDateString.toString()
+                    },
                     year, month, day)
             datePickerDialog.show()
         }
 
         this.addStop.setOnClickListener {
-            val addedLayout: LinearLayout = findViewById(R.id.linearLayoutStopPoint) as LinearLayout
-            addedLayout.orientation = LinearLayout.HORIZONTAL
-            addedLayout.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    50)
+            val myLayout = findViewById(R.id.mainLayout) as LinearLayout
+            val hiddenInfo: LinearLayout = layoutInflater.inflate(R.layout.hidden, myLayout, false) as LinearLayout
+            myLayout.addView(hiddenInfo)
+
+            //val addedLayout: LinearLayout = findViewById(R.id.linearLayoutStopPoint) as LinearLayout
+            // = LinearLayout(applicationContext)
+            //addedLayout.orientation = LinearLayout.HORIZONTAL
+            //addedLayout.layoutParams = LinearLayout.LayoutParams(
+            //        LinearLayout.LayoutParams.MATCH_PARENT,
+            //        50)
             //val destination = AutoCompleteTextView(applicationContext)
             //addedLayout.addView(destination)
-            arrayList.add(addedLayout)
-            adapter!!.notifyDataSetChanged()
+            //arrayList.add(hiddenInfo)
+            //adapter!!.notifyDataSetChanged()
         }
 
     }
