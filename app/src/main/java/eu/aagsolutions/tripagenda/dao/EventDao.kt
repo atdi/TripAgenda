e88aa24ba85e650259dc32e3affe608e63066dfd
atdi <aurel.avramescu@gmail.com>
2017-09-15 09:50:52 +0200

    draft dao

--- app/src/main/java/eu/aagsolutions/tripagenda/dao/EventDao.kt
+++ app/src/main/java/eu/aagsolutions/tripagenda/dao/EventDao.kt
@@ -3,6 +3,7 @@ package eu.aagsolutions.tripagenda.dao
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
 import eu.aagsolutions.tripagenda.model.Event
 
 
@@ -14,4 +15,10 @@ interface EventDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addEvent(event: Event)
+
+    @Query("select * from event where id = :id")
+    fun findById(id: Long): Event
+
+
+
 }
