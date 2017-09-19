2aabd3972b6d67964c51f6af25e30c94c558ab71
avramesc <aurel.avramescu@tomtom.com>
2017-09-19 14:30:12 +0200

    Set to trip the start date

--- app/src/main/java/eu/aagsolutions/tripagenda/AgendaActivity.kt
+++ app/src/main/java/eu/aagsolutions/tripagenda/AgendaActivity.kt
@@ -82,12 +82,10 @@ class AgendaActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLi
         setContentView(R.layout.activity_agenda)
 
         val c = Calendar.getInstance()
-        val hour = c.get(Calendar.HOUR_OF_DAY)
-        val minute = c.get(Calendar.MINUTE)
         val day = c.get(Calendar.DAY_OF_MONTH)
         val month = c.get(Calendar.MONTH)
         val year = c.get(Calendar.YEAR)
-        startCalendar.set(year, month, day, hour, minute)
+        startCalendar.set(year, month, day, 0, 0)
         startDateString.append(day).append("/").append(month).append("/").append(year)
         this.btnDate.text = startDateString.toString()
 
