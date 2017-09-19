c0b15b8b3fb2edc808c52b3fc33d5c3c6dc51e69
avramesc <aurel.avramescu@tomtom.com>
2017-09-19 16:55:05 +0200

    add new activity

---
+++ app/src/main/java/eu/aagsolutions/tripagenda/TripViewActivity.kt
@@ -0,0 +1,12 @@
+package eu.aagsolutions.tripagenda
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TripViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trip_view)
+    }
+}
