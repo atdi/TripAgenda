9b08a1dc837e98d6acd6970fca700ebd12e03b9a
avramesc <aurel.avramescu@tomtom.com>
2017-09-25 12:12:04 +0200

    fix the agenda display

--- app/src/main/java/eu/aagsolutions/tripagenda/TripViewActivity.kt
+++ app/src/main/java/eu/aagsolutions/tripagenda/TripViewActivity.kt
@@ -1,13 +1,11 @@
 package eu.aagsolutions.tripagenda
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import eu.aagsolutions.tripagenda.dao.AppDatabase
 import eu.aagsolutions.tripagenda.services.TripService
-import kotlinx.android.synthetic.main.activity_trip_view.endAddress
 import kotlinx.android.synthetic.main.activity_trip_view.eventsList
-import kotlinx.android.synthetic.main.activity_trip_view.startAddress
 import java.text.SimpleDateFormat
 
 class TripViewActivity : AppCompatActivity() {
@@ -24,8 +22,7 @@ class TripViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_trip_view)
         val tripId = intent.getSerializableExtra("tripId") as String
         val trip = tripService.findById(tripId)
-        this.startAddress.text = trip.startPoint.address
-        this.endAddress.text = trip.endPoint.address
+        arrayList.add(trip.startPoint.address)
         val events = tripService.findEventsByTripId(tripId)
         events.forEach { e ->
             run {
@@ -33,6 +30,7 @@ class TripViewActivity : AppCompatActivity() {
                 arrayList.add(String.format("%s %s", e.point.address, dateTime))
             }
         }
+        arrayList.add(trip.endPoint.address)
         adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, arrayList)
         eventsList.adapter = adapter
 
